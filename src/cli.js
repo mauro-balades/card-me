e3854226d175fc9ea812aa578694d7ca27eb3a47
mauro-balades <mauro.balades@tutanota.com>
2021-12-15 19:03:01 +0100

    started card CLI

---
+++ src/cli.js
@@ -0,0 +1,120 @@
+// import files from "./files.js";
+import prompts from "prompts";
+import boxen from "boxen";
+import fs from "fs";
+import path from "path";
+
+const get_information = async () => {
+    return new Promise(async (resolve, reject) => {
+        const response = await prompts([
+            {
+                type: "text",
+                name: "name",
+                message: `What's your first name?`,
+                validate: (response) =>
+                    response.length < 1 ? `Add a valid name` : true,
+            },
+            {
+                type: "select",
+                name: "face",
+                message: "Pick a face!",
+                choices: [
+                    { title: "ツ", value: "ツ" },
+                    { title: "˙ ͜ʟ˙", value: "˙ ͜ʟ˙" },
+                    { title: "╚(ಠ_ಠ)=┐", value: "╚(ಠ_ಠ)=┐" },
+                    { title: "(¬_¬)", value: "(¬_¬)" },
+                    { title: "｡◕‿◕｡", value: "｡◕‿◕｡" },
+                    { title: "ಠ~ಠ", value: "ಠ~ಠ" },
+                    { title: "^̮^", value: "^̮^" },
+                    { title: "ʕ•ᴥ•ʔ", value: "ʕ•ᴥ•ʔ" },
+                ],
+            },
+            {
+                type: "text",
+                name: "work",
+                message: `In what are you working right now?`,
+                initial: null,
+            },
+            {
+                type: "text",
+                name: "github",
+                message: `What's your GitHub username?`,
+                initial: null,
+            },
+            {
+                type: "text",
+                name: "devto",
+                message: `What is your Dev.to name?`,
+                initial: null,
+            },
+            {
+                type: "text",
+                name: "linkedIn",
+                message: `What is your current LinkedIn profile?`,
+                initial: null,
+            },
+            {
+                type: "text",
+                name: "twitter",
+                message: `What's your twitter handle?`,
+                initial: null,
+            },
+        ]);
+
+        resolve(response);
+    });
+};
+
+const prepare_card = (data) => {
+    var res = [];
+
+    for (const [key, value] of Object.entries(data)) {
+        if (value == "") continue;
+
+        switch (key) {
+            case "work":
+                res.push(`    Work : ${value}`);
+                break;
+
+            case "github":
+                res.push(`  GitHub : ${value}`);
+                break;
+
+            case "devto":
+                res.push(`  Dev.to : ${value}`);
+                break;
+
+            case "linkedIn":
+                res.push(`LinkedIn : ${value}`);
+                break;
+
+            case "twitter":
+                res.push(` Twitter : ${value}`);
+                break;
+        }
+    }
+
+    return res.join("\n");
+};
+
+export default async () => {
+    let response = await get_information();
+    let title = `${response.name}'s card`;
+    let folder = `${response.name}-card`
+
+    let card = prepare_card(response);
+
+    fs.mkdirSync(folder);
+    fs.writeFile(`${folder}/README.md`, README.replace(/\[\[NAME\]\]/, response.name))
+    fs.writeFile(`${folder}/package.json`, PACKAGE.replace(/\[\[NAME\]\]/, response.name).toLocaleLowerCase())
+    fs.writeFile(`${folder}/index.js`, PACKAGE.replace(/\[\[CARD\]\]/, card))
+
+    console.log(
+        boxen(card, {
+            padding: 2,
+            margin: 1,
+            title: title,
+            titleAlignment: "center",
+        })
+    );
+};
---
+++ src/files.js
@@ -0,0 +1,34 @@
+
+const README = `
+# Welcome to [[NAME]]'s card
+
+This is my own personal card!
+
+* Made thanks by [card-me]()
+`
+
+const PACKAGE = `
+{
+    "name": "card-[[NAME]]",
+    "version": "1.0.0",
+    "description": "This is my personal card!",
+    "main": "index.js",
+    "bin": {
+      "card-[[NAME]]": "./index.js"
+    },
+    "type": "module",
+    "keywords": [],
+    "author": "",
+    "license": "ISC",
+    "dependencies": {
+      "boxen": "^6.2.1",
+      "prompts": "^2.4.2"
+    }
+  }
+
+`
+
+const INDEX = `
+#!/usr/bin/env node
+console.log(\`[[CARD]]\`) // DO NOT EDIT
+`
